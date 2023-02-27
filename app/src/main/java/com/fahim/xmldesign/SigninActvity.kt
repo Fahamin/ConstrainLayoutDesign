0cbe73cd3273e9a0c72401f5cc1768c96049d64c
Fahamin <fahamin00@gmail.com>
2023-02-27 12:37:11 +0600

    Initial commit

---
+++ app/src/main/java/com/fahim/xmldesign/SigninActvity.kt
@@ -0,0 +1,11 @@
+package com.fahim.xmldesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SigninActvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin_actvity)
+    }
+}
---
+++ app/src/main/java/com/fahim/xmldesign/WellComeActivity.kt
@@ -0,0 +1,11 @@
+package com.fahim.xmldesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WellComeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_well_come)
+    }
+}
